87ff3e4ebefb82ae2a8fb8612299eee6fa39c009
akempins <01178535@pw.edu.pl>
2023-07-28 15:24:05 +0200

    Added fake cv to return from GET

--- src/main/java/com/cvizard/pdfcreator/controller/ResumeController.java
+++ src/main/java/com/cvizard/pdfcreator/controller/ResumeController.java
@@ -34,7 +34,8 @@ public class ResumeController {
     public ResponseEntity<Resource> getPdfFile(@RequestParam(name = "key") String key) throws IOException, DocumentException {
         Resume resume = resumeRepository.findById(key).orElseThrow();
         resumeService.createPdf(key, resume);
-        File file = new File("resources/" + key + ".pdf");
+//        File file = new File("resources/" + key + ".pdf");
+        File file = new File("resources/mocked/cv.pdf");
         Resource resource = new FileSystemResource(file);
         return ResponseEntity.ok().body(resource);
     }
