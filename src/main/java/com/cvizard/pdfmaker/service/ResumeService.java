8ecd05f99b5ee05be928ccb505c5b3b1a514c602
Paweł Solecki <pawelll.solecki@gmail.com>
2023-08-21 10:58:07 +0200

    add pl template

--- src/main/java/com/cvizard/pdfmaker/service/ResumeService.java
+++ src/main/java/com/cvizard/pdfmaker/service/ResumeService.java
@@ -9,6 +9,9 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.text.DocumentException;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -16,7 +19,6 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.*;
 import java.io.FileOutputStream;
-import java.net.URISyntaxException;
 
 @Service
 @AllArgsConstructor
@@ -26,12 +28,40 @@ public class ResumeService {
     private final ITextRenderer renderer = new ITextRenderer();
     private final Context context = new Context();
 
-    public void createPdf(String key, Resume resume) throws IOException, DocumentException {
+    public ResponseEntity<?> createResponse(Resume resume, String key, String template) throws DocumentException, IOException {
+        ResponseEntity<?> responseEntity;
+        switch (resume.getStatus()) {
+            case READY: {
+                createPdf(key, resume, template);
+                File file = new File("resources/" + key + ".pdf");
+                Resource resource = new FileSystemResource(file);
+                responseEntity = ResponseEntity.status(200).body(resource);
+//                file.delete();
+//                resumeRepository.delete(resume);
+                break;
+            }
+            case PROCESSING: {
+                responseEntity = ResponseEntity.status(425).body(null);
+                break;
+            }
+            case ERROR: {
+                responseEntity = ResponseEntity.status(404).body(null);
+                break;
+            }
+            default: {
+                responseEntity = ResponseEntity.status(422).body(null);
+                break;
+            }
+        }
+        return responseEntity;
+    }
+
+    public void createPdf(String key, Resume resume, String template) throws IOException, DocumentException {
         FileOutputStream fos = new FileOutputStream("resources/"+key+"-logo.pdf");
         Image img = new Image(ImageDataFactory.create("/app/resources/logo.png"));
 
         context.setVariable("resume",resume);
-        String processed = templateEngine.process("resume", context);
+        String processed = templateEngine.process("resume"+template, context);
         renderer.setDocumentFromString(processed);
         renderer.layout();
         renderer.createPDF(fos);
